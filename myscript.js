d9b462d702f9325f7500500fa022166f5d30027e
M1-89-loulwah <76619506+M1-89-loulwah@users.noreply.github.com>
2021-01-07 21:32:47 +0300

    cw-8-done

--- myscript.js
+++ myscript.js
@@ -1,14 +1,53 @@
 let board = [
-    ['00', '01', '02'],
-    ['10', '11', '12'],
-    ['20', '21', '22']
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
 ]
 
 
-
-function press(cellNO) {
+let turn = "O";
+function press(x, y) {
 
     // Do stuff
-    console.log(cellNO)
+    if (turn == "O") {
+        turn = "X"
+    }else {turn = "O"}
+    // console.log(x,y)
+    board[x][y] = turn;
+    let cell = document.getElementById(`${x}${y}`);
+    cell.innerText = turn;
+
+    if(board[0][0] == turn && board[0][1] == turn && board[0][2] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    } 
+    if(board[1][0] == turn && board[1][1] == turn && board[1][2] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[2][0] == turn && board[2][1] == turn && board[2][2] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[0][0] == turn && board[1][0] == turn && board[2][0] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[0][1] == turn && board[1][1] == turn && board[2][1] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[0][2]== turn && board[1][2] == turn && board[2][2] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[0][0] == turn && board[1][1] == turn && board[2][2] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+    if(board[0][2] == turn && board[1][1] == turn && board[2][0] == turn){
+        document.getElementById("B").innerText = `Bravooo ${turn}`
+    }
+
+
+     
     
+    
+}
+
+function myButton(){
+    location.reload();
 }
